60bc3fa1e7ebc039a940000b5369ac810d69c2ad
Tarikul Islam Nahid <jttarekulnahid@gmail.com>
2022-06-12 10:55:24 +0600

    migrate create for blogs and blogCategory

---
+++ app/Http/Controllers/BlogCategoriesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Admin\blogCategories;
+use Illuminate\Http\Request;
+
+class BlogCategoriesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Admin\blogCategories  $blogCategories
+     * @return \Illuminate\Http\Response
+     */
+    public function show(blogCategories $blogCategories)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Admin\blogCategories  $blogCategories
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(blogCategories $blogCategories)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Admin\blogCategories  $blogCategories
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, blogCategories $blogCategories)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Admin\blogCategories  $blogCategories
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(blogCategories $blogCategories)
+    {
+        //
+    }
+}
---
+++ database/migrations/2022_06_11_164739_create_blogs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->mediumText('title');
+            $table->mediumText('slug')->unique();
+            $table->text('description');
+            $table->string('thumb');
+            $table->integer('views')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blogs');
+    }
+};
